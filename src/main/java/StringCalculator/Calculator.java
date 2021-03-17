9ae35f2ccd049c3842480552ff1decee1e53e847
Jonas <jonasgranbom@hotmail.com>
2021-03-17 14:46:52 +0100

    step 2 commit

--- src/main/java/StringCalculator/Calculator.java
+++ src/main/java/StringCalculator/Calculator.java
@@ -7,9 +7,7 @@ public class Calculator {
         int returnVal = 0;
         String[] numbersArray = numbers.split(",");
 
-        if (numbersArray.length > 2) {
-            throw new RuntimeException("Up to two numbers separated by comma is allowed");
-        }
+
         for (String number : numbersArray) {
 
             returnVal += Integer.parseInt(number.trim());
--- src/test/java/StringCalculator/CalculatorTest.java
+++ src/test/java/StringCalculator/CalculatorTest.java
@@ -7,12 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void addTestMethod(){
+    void addMultipleNumbersTestMethod() {
         Calculator calculator = new Calculator();
-        assertEquals(1+2, calculator.add("1, 2"));
 
+        assertEquals(1 + 2 + 2 + 4, calculator.add("1, 2, 2, 4"));
 
+    }
 
+    @Test
+    void addSingleNumberTestMethod() {
+        Calculator calc = new Calculator();
+
+        assertEquals(5, calc.add("5"));
 
     }
 
